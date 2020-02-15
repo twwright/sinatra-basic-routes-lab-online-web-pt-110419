bcdaeae5cb9faa81f823b7a392ef76fbfd406820
Thomas Wright <twwright@gmail.com>
2020-02-15 22:57:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	 get '/' do
+		puts "Hello, World!"
+	 end
+
+	 get '/name' do
+		 puts "My name is Thomas!"
+	 end
+
+	 get '/hometown' do
+		puts "My hometown is Sewell, NJ!"
+	 end
+
+	 get '/favorite-song' do
+		 puts "My favorite song is 'Fallen Empires' by Snow Patrol!"
+	 end
+
 end
